612b15ef9d2cbdf4aad3134214c031a1da70f4f1
LeandroOnline <leandrosavat@gmail.com>
2023-09-27 16:11:35 -0300

    feat: navbar style

--- app/Components/Navbar.jsx
+++ app/Components/Navbar.jsx
@@ -1,16 +1,29 @@
 import Link from "next/link";
 import style from "./Navbar.module.css";
+import Image from "next/image";
 
 const Navbar = () => {
   return (
     <nav className={style.navContainer}>
       <h1 className={style.title}>NORA</h1>
       <section className={style.menu}>
-        <Link href="/inicio" className={style.link}>Inicio</Link>
-        <Link href="/inicio" className={style.link}>Acerca de Mi</Link>
-        <Link href="/inicio" className={style.link}>Decodificacion</Link>
-        <Link href="/inicio" className={style.link}>Terapias Holisticas</Link>
+        <Link href="/inicio" className={style.link}>
+          Inicio
+        </Link>
+        <Link href="/inicio" className={style.link}>
+          Acerca de Mi
+        </Link>
+        <Link href="/inicio" className={style.link}>
+          Decodificacion
+        </Link>
+        <Link href="/inicio" className={style.link}>
+          Terapias Holisticas
+        </Link>
       </section>
+      <button className={style.wspButton}>
+        <Image src="/images/whatsapp.png" alt="wsp" width={27} height={27} />
+        WhatsApp
+      </button>
     </nav>
   );
 };
