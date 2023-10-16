832f42d5c343d0464682cf3367a9ca2bdb3b44d3
LeandroOnline <leandrosavat@gmail.com>
2023-10-16 14:02:46 -0300

    feat: start Therapy component

--- app/Components/Therapy.jsx
+++ app/Components/Therapy.jsx
@@ -1,8 +1,57 @@
+"use client";
+import { useState } from "react";
+import style from "./Therapy.module.css";
+
 const Therapy = () => {
+  const [state, setState] = useState("therapy");
+
   return (
     <div className="allscreen allContainer">
       <h1 className="title">Decodificación Bioemocional</h1>
-      <div className="bigcard">hola</div>
+      <div className="bigcard">
+        <div>
+          <button
+            className={
+              state === "therapy" ? style.buttonActive : style.buttonInactive
+            }
+            onClick={() => setState("therapy")}
+          >
+            Terapias Holisticas
+          </button>
+          <button
+            className={
+              state === "number" ? style.buttonActive : style.buttonInactive
+            }
+            onClick={() => setState("number")}
+          >
+            Numerologia
+          </button>
+          <button
+            className={
+              state === "tarot" ? style.buttonActive : style.buttonInactive
+            }
+            onClick={() => setState("tarot")}
+          >
+            Tarot Evolutivo
+          </button>
+          <button
+            className={
+              state === "pendulum" ? style.buttonActive : style.buttonInactive
+            }
+            onClick={() => setState("pendulum")}
+          >
+            Pendulo Hebreo
+          </button>
+          <button
+            className={
+              state === "reiki" ? style.buttonActive : style.buttonInactive
+            }
+            onClick={() => setState("reiki")}
+          >
+            Reiki Master
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
