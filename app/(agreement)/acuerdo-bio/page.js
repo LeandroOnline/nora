be51737d324a1de058f336714de902e3fa7df8f6
LeandroOnline <leandrosavat@gmail.com>
2023-10-18 12:40:19 -0300

    feat: test

--- app/(agreement)/acuerdo-bio/page.js
+++ app/(agreement)/acuerdo-bio/page.js
@@ -1,11 +1,24 @@
+import style from "../page.module.css";
+
 const Agreement = () => {
   return (
     <div className="allscreen">
       <div className="bigcard">
-        <form>
-          <div>
+        <form className={style.form}>
+          <div className={style.inputContainer}>
             <p>Nombre completo</p>
-            <input placeholder="..."/>
+            <input placeholder="..." className={style.inputName} />
+          </div>
+          <div className={style.inputContainer}>
+            <p>Fecha de nacimiento</p>
+            <input placeholder="**/**/****" className={style.inputName} />
+          </div>
+          <div className={style.inputContainer}>
+            <p>Modalidad</p>
+            <select className={style.select} >
+              <option>▾ Online</option>
+              <option>▾ Presencial</option>
+            </select>
           </div>
         </form>
       </div>
