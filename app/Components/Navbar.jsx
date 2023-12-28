6ad6935eabb74f75e402b3cce850461394dbe679
LeandroOnline <leandrosavat@gmail.com>
2023-12-27 21:17:09 -0300

    feat: responsive navbar

--- app/Components/Navbar.jsx
+++ app/Components/Navbar.jsx
@@ -2,29 +2,36 @@
 import Link from "next/link";
 import style from "./Navbar.module.css";
 import Image from "next/image";
+import { useState } from "react";
 
 const Navbar = () => {
+  const [state, setState] = useState(false);
   return (
     <nav className={style.navContainer}>
       <h1 className={style.title}>NORA</h1>
 
       <Image
-        className={style.menuButton}
+        className={state ? style.none : style.menuButton}
         src="/images/menu.png"
         alt="wsp"
         width={27}
         height={27}
+        onClick={() => setState(true)}
       />
 
       <Image
-        className={style.menuButton}
+        className={state ? style.xButton : style.none}
         src="/images/x.png"
         alt="wsp"
         width={27}
         height={27}
+        onClick={() => setState(false)}
       />
 
-      <section className={style.menu}>
+      <section
+        className={state ? style.menu : style.menuOff}
+        onClick={() => setState(false)}
+      >
         <Link href="/" className={style.link}>
           Inicio
         </Link>
