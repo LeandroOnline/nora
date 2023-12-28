6b81bd934c61999726bd3c0f833a0d7c46bfe9d1
LeandroOnline <leandrosavat@gmail.com>
2023-12-27 21:01:39 -0300

    feat: start responsive, header done

--- app/Components/Navbar.jsx
+++ app/Components/Navbar.jsx
@@ -1,3 +1,4 @@
+"use client";
 import Link from "next/link";
 import style from "./Navbar.module.css";
 import Image from "next/image";
@@ -6,6 +7,23 @@ const Navbar = () => {
   return (
     <nav className={style.navContainer}>
       <h1 className={style.title}>NORA</h1>
+
+      <Image
+        className={style.menuButton}
+        src="/images/menu.png"
+        alt="wsp"
+        width={27}
+        height={27}
+      />
+
+      <Image
+        className={style.menuButton}
+        src="/images/x.png"
+        alt="wsp"
+        width={27}
+        height={27}
+      />
+
       <section className={style.menu}>
         <Link href="/" className={style.link}>
           Inicio
