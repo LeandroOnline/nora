0d53d3317c847470faa785f674cdd76f06d47ad0
LeandroOnline <leandrosavat@gmail.com>
2024-01-12 21:07:36 -0300

    fix: wsp message

--- helpers/sendWsp.js
+++ helpers/sendWsp.js
@@ -19,7 +19,7 @@ export const sendWsp = (
           alert("El mes debe estar en el rango de 1 a 12.");
         } else {
           window.open(
-            `${wame}★ Solicitud de Biodecodificacion ★%0D%0A%0D%0A✦ Nombre:%20${name}%0D%0A✦ Fecha%20de%20Nacimiento:%20${date}%0D%0A✦ Modalidad:%20${option}%0D%0A✦ Síntomas:%20${sintoma}%20 %0D%0A✓ ${confirm}`,
+            `${wame}★ Solicitud de Biodecodificacion ★%0D%0A%0D%0A✦ Nombre:%20${name}%0D%0A✦ Fecha%20de%20Nacimiento:%20${date}%0D%0A✦ Modalidad:%20${option}%0D%0A✦ Síntomas:%20${sintoma}%20 %0D%0A✦ MANIFIESTO DE CONFORMIDAD: En búsqueda de mi bienestar físico, mental y emocional manifiesto mi voluntad de iniciar las consultas en Bioexistencia Consciente con la Consultora NORA BENITO, siendo totalmente responsable de mi decisión sabiendo que dichas consultas no reemplazan ni pretenden ser una práctica médica ni  psiquiátrica. Manifiesto no tener patologías cardíacas, ni psiquiátricas como bipolaridad, depresión ni esquizofrenia, ni haber tenido ACV y/o estar cursando embarazo. Si reconozco algunos de estas condiciones sé que un “tercero podrá concurrir por mí a la sesión que se llama “Atención a través de un tercero” ya que todos somos uno y al sanar él, sano yo, me comprometo a avisar a la Consultora.`,
             "_blank"
           );
         }
