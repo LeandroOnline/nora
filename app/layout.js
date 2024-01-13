6da74383d8dcc76e7648ce5706dfa69099e5e1a9
LeandroOnline <leandrosavat@gmail.com>
2024-01-13 00:55:59 -0300

    feat: new icon page

--- app/layout.js
+++ app/layout.js
@@ -2,7 +2,6 @@ import Navbar from "./Components/Navbar";
 import "./globals.css";
 import { Dosis } from "next/font/google";
 
-
 const dosis = Dosis({ subsets: ["latin"], weight: "600" });
 
 export const metadata = {
@@ -11,7 +10,6 @@ export const metadata = {
 };
 
 export default function RootLayout({ children }) {
-  
   return (
     <html lang="en" className="Background">
       <body className={dosis.className}>
